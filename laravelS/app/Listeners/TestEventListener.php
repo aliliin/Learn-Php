bd2cb16bc4507ff665dba2348ea127bce433e0d1
aliliin <phperali@gmail.com>
2020-04-05 18:01:11 +0800

    异步事件监听和处理

---
+++ laravelS/app/Listeners/TestEventListener.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Listeners;
+
+
+use Hhxsv5\LaravelS\Swoole\Task\Listener;
+use Illuminate\Support\Facades\Log;
+
+class TestEventListener extends Listener
+{
+    protected $event;
+
+    public function handle()
+    {
+        Log::info(__CLASS__ . ': 开始处理', [$this->event->getData()]);
+        sleep(3); // 模拟耗时代码的执行
+        Log::info(__CLASS__ . ': 处理完毕');
+    }
+}
